14f372a53d72626adb513535423148ccddbce799
vitorhaoliveira <vitorhaoliveira123@gmail.com>
2024-11-07 23:01:27 -0300

    ajustes estilição classes

--- src/routes/stackClasses.routes.tsx
+++ src/routes/stackClasses.routes.tsx
@@ -1,9 +1,11 @@
 import "react-native-gesture-handler";
 import { createStackNavigator } from "@react-navigation/stack";
-import Classes from "../pages/StackClassesPages/Classes";
-import RegisterClasses from "../pages/StackClassesPages/RegisterClasses";
-import ClassDetails from "../pages/StackClassesPages/ClassDetails";
+
 import React from "react";
+import Classes from "../pages/Classes";
+import RegisterClasses from "../pages/RegisterClasses";
+import ClassDetails from "../pages/ClassDetails";
+
 
 const Stack = createStackNavigator();
 
