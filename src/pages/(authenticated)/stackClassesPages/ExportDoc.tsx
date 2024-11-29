32ced1519031f0a05afeb348daa54f0fbf1fb474
marcs13013 <marcus.elias@etec.sp.gov.br>
2024-11-29 20:30:07 -0300

    página de exportar documento atualizada

--- src/pages/(authenticated)/stackClassesPages/ExportDoc.tsx
+++ src/pages/(authenticated)/stackClassesPages/ExportDoc.tsx
@@ -16,6 +16,15 @@ interface StudentData {
   nomeAluno: string;
   nascimentoAluno: string;
   rmAluno: string;
+  turmaId: string; // Adicionei turmaId para associar o aluno à turma
+}
+
+interface ClassData {
+  id: string;
+  nomeTurma: string;
+  periodoTurma: string;
+  educationLevel: string;
+  school: string;
 }
 
 const Container = styled.View`
@@ -37,7 +46,37 @@ export default function ExportDoc({ navigation, route }) {
   const [namefile, setFilename] = useState('');
   const [dadosPerfil, setDadosPerfil] = useState(null);
   const [students, setStudents] = useState<StudentData[]>([]);
-  const { turmaId } = route.params; // Obtendo o parâmetro turmaId
+  const [turmas, setTurmas] = useState<ClassData[]>([]);
+  const turmaId = route.params?.turmaId; // Obtendo turmaId diretamente de route.params
+
+  useEffect(() => {
+    if (!turmaId) {
+      console.error("turmaId não encontrado");
+      return;
+    }
+
+    const unsubscribe = onSnapshot(
+      doc(db, "tblTurma", turmaId), // Acesso direto ao documento da turma pela referência
+      (docSnapshot) => {
+        if (docSnapshot.exists()) {
+          const data = docSnapshot.data();
+          setTurmas([
+            {
+              id: docSnapshot.id,
+              nomeTurma: data.nomeTurma,
+              periodoTurma: data.periodoTurma,
+              educationLevel: data.educationLevel,
+              school: data.school,
+            },
+          ]);
+        } else {
+          console.error("Turma não encontrada.");
+        }
+      }
+    );
+
+    return () => unsubscribe();
+  }, [turmaId]);
 
   useEffect(() => {
     const unsubscribeAuth = auth.onAuthStateChanged((user) => {
@@ -64,74 +103,82 @@ export default function ExportDoc({ navigation, route }) {
   }, []); // Esse useEffect só executa uma vez após o componente ser montado
 
   useEffect(() => {
-    const unsubscribe = onSnapshot(
-      query(
-        collection(db, "tblAluno"),
-        where("turmaRef", "==", doc(db, "tblTurma", turmaId)),
-        orderBy("nomeAluno") // Ordenando pela propriedade nomeAluno
-      ),
-      (querySnapshot) => {
-        const studentList: StudentData[] = [];
-        querySnapshot.forEach((docSnap) => {
-          const data = docSnap.data();
-          studentList.push({
-            id: docSnap.id,
-            nomeAluno: data.nomeAluno,
-            nascimentoAluno: data.nascimentoAluno,
-            rmAluno: data.rmAluno,
+    if (turmas.length > 0) {
+      const turmaIds = turmas.map(turma => turma.id);
+      const unsubscribe = onSnapshot(
+        query(
+          collection(db, "tblAluno"),
+          where("turmaRef", "in", turmaIds),
+          orderBy("nomeAluno") // Ordenando pela propriedade nomeAluno
+        ),
+        (querySnapshot) => {
+          const studentList: StudentData[] = [];
+          querySnapshot.forEach((docSnap) => {
+            const data = docSnap.data();
+            studentList.push({
+              id: docSnap.id,
+              nomeAluno: data.nomeAluno,
+              nascimentoAluno: data.nascimentoAluno,
+              rmAluno: data.rmAluno,
+              turmaId: data.turmaRef.id // Adiciona o turmaId ao aluno
+            });
           });
-        });
 
-        setStudents(studentList);
-      }
-    );
+          setStudents(studentList);
+        }
+      );
 
-    return () => unsubscribe();
-  }, [turmaId]);
-
-  if (!turmaId) {
-    return (
-      <View>
-        <Text>Erro: turmaId não foi fornecido!</Text>
-      </View>
-    );
-  }
+      return () => unsubscribe();
+    }
+  }, [turmas]);
 
   const generateExcel = () => {
+    if (turmas.length === 0) {
+      console.error('Turmas não encontradas');
+      return;
+    }
+
     const wb = XLSX.utils.book_new();
-  
-    // Cabeçalho da planilha
-    const header = ['Nome do Aluno', 'RM','1° Sondagem','2° Sondagem','3° Sondagem','4° Sondagem',];
-    
-    // Adiciona uma linha para o nome do professor
-    const professorRow = ['', '', dadosPerfil?.nomeProfessor || ''];
-  
-    // Adiciona uma linha em branco
-    const emptyRow = ['', '', ''];
-  
-    // Mapeia os alunos para os dados da planilha
-    const data = students.map(student => [
-      student.nomeAluno, 
-      student.rmAluno
-    ]);
-  
-    // Cria a planilha com o nome do professor, linhas vazias e os dados dos alunos
-    const ws = XLSX.utils.aoa_to_sheet([
-      professorRow,   // Linha com o nome do professor
-      emptyRow,       // Linha em branco
-      header,         // Cabeçalho
-      ...data         // Dados dos alunos
-    ]);
-  
-    // Adiciona a planilha ao arquivo
-    XLSX.utils.book_append_sheet(wb, ws, 'Alunos');
-  
+
+    turmas.forEach((turma) => {
+      // Cabeçalho da planilha
+      const header = ['Nome do Aluno', 'RM', '1° Bimestre', '2° Bimestre', '3° Bimestre', '4° Bimestre'];
+
+      // Usando o nome da turma que foi corretamente obtida
+      const turmaName = turma?.nomeTurma || 'Turma Desconhecida';
+
+      // Adiciona uma linha para o nome do professor e da turma
+      const professorRow = ['', '', (dadosPerfil?.nomeProfessor + " - " + turmaName) || ''];
+
+      // Adiciona uma linha em branco
+      const emptyRow = ['', '', ''];
+
+      // Filtra os alunos da turma atual
+      const studentsData = students
+        .filter(student => student.turmaId === turma.id)
+        .map(student => [
+          student.nomeAluno,
+          student.rmAluno
+        ]);
+
+      // Cria a planilha com o nome do professor, linhas vazias e os dados dos alunos
+      const ws = XLSX.utils.aoa_to_sheet([
+        professorRow,   // Linha com o nome do professor e turma
+        emptyRow,       // Linha em branco
+        header,         // Cabeçalho
+        ...studentsData // Dados dos alunos
+      ]);
+
+      // Adiciona a planilha ao arquivo
+      XLSX.utils.book_append_sheet(wb, ws, turmaName);
+    });
+
     // Converte o arquivo para base64
     const base64 = XLSX.write(wb, { type: 'base64' });
-  
+
     // Cria o caminho do arquivo
     const filename = FileSystem.documentDirectory + `${namefile}.xlsx`;
-  
+
     // Salva o arquivo no sistema de arquivos
     FileSystem.writeAsStringAsync(filename, base64, {
       encoding: FileSystem.EncodingType.Base64,
@@ -140,8 +187,6 @@ export default function ExportDoc({ navigation, route }) {
       Sharing.shareAsync(filename);
     });
   };
-  
-  
 
   return (
     <Container>
@@ -169,7 +214,7 @@ export default function ExportDoc({ navigation, route }) {
       <View style={styles.BtnView}>
         <Btn
           onPress={() =>
-            navigation.navigate('EditSondagem', { turmaId })
+            navigation.navigate('EditSondagem', { turmaId: route.params.turmaId })
           }
           texto="Editar Sondagens"
         />
