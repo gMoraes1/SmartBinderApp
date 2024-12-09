84bacb52e8f5271180b1839e58f1e7e427a6b4fc
marcs13013 <marcus.elias@etec.sp.gov.br>
2024-12-09 19:49:19 -0300

    tabnavigator arrumado

--- src/routes/tab.routes.js
+++ src/routes/tab.routes.js
@@ -1,9 +1,6 @@
 import "react-native-gesture-handler";
-import { NavigationContainer } from "@react-navigation/native";
-import { createStackNavigator } from "@react-navigation/stack";
-import {createBottomTabNavigator} from '@react-navigation/bottom-tabs'
-import { Feather, Ionicons } from "@expo/vector-icons";
-import { StyleSheet, Text, View, ScrollView, Image } from 'react-native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { Ionicons } from "@expo/vector-icons";
 import styled from "styled-components/native";
 import Home from "../pages/(authenticated)/stackHomePages/home";
 import StackCalendar from "./stackCalendar.routes";
@@ -11,12 +8,7 @@ import StackProfile from "./stackProfile";
 import StackRegister from "./stackRegister.routes";
 import StackSettings from "./stackSetting.routes";
 
-
-
-
 const Tab = createBottomTabNavigator();
-const Stack = createStackNavigator();
-
 
 const Container = styled.View`
   background-color: ${props => props.theme.background};
@@ -25,88 +17,89 @@ const Container = styled.View`
 `;
 
 export default function TabNavigator() {
-    return (
-      <Container>
-      <Tab.Navigator screenOptions={{
-        headerShown: false,
-        tabBarShowLabel:false,
-        tabBarLabel:false,
-        tabBarShowLabel:false,
-        tabBarStyle: {
-          position: 'absolute',
-          height: '8%',
-          borderTopWidth: 1,
-        },
-      }}>
-  
-  
+  return (
+    <Container>
+      <Tab.Navigator
+        screenOptions={{
+          headerShown: false,
+          tabBarShowLabel: false,
+          tabBarIconStyle:{
+           margin:'13%',
+          },
+          tabBarStyle: {
+            position: 'absolute',
+            height: '8.5%', // Altura da barra
+            borderTopWidth: 1,
+            justifyContent: 'center', // Centraliza o conteúdo verticalmente
+            alignItems: 'center',     // Centraliza o conteúdo horizontalmente
+          },
+        }}
+      >
         <Tab.Screen
           name="home"
           component={Home}
           options={{
             tabBarIcon: ({ color, size, focused }) => {
               if (focused) {
-                return <Ionicons name="home" size={29} color={"#2E1966"} />
+                return <Ionicons name="home" size={30} color={"#2E1966"} />;
               }
-              return <Ionicons name="home-outline" size={29} color={"#6939E9"} />
-            }
+              return <Ionicons name="home-outline" size={30} color={"#6939E9"} />;
+            },
           }}
         />
-  
+
         <Tab.Screen
           name="calendar"
           component={StackCalendar}
           options={{
             tabBarIcon: ({ color, size, focused }) => {
               if (focused) {
-                return <Ionicons name="calendar" size={29} color={"#2E1966"} />
+                return <Ionicons name="calendar" size={30} color={"#2E1966"} />;
               }
-              return <Ionicons name="calendar-outline" size={29} color={"#6939E9"} />
-            }
+              return <Ionicons name="calendar-outline" size={30} color={"#6939E9"} />;
+            },
           }}
         />
-  
+
         <Tab.Screen
           name="profile"
           component={StackProfile}
           options={{
             tabBarIcon: ({ color, size, focused }) => {
               if (focused) {
-                return <Ionicons name="person" size={29} color={"#2E1966"} />
+                return <Ionicons name="person" size={30} color={"#2E1966"} />;
               }
-              return <Ionicons name="person-outline" size={29} color={"#6939E9"} />
-            }
+              return <Ionicons name="person-outline" size={30} color={"#6939E9"} />;
+            },
           }}
         />
-  
+
         <Tab.Screen
           name="Turmas"
           component={StackRegister}
           options={{
-  
             tabBarIcon: ({ color, size, focused }) => {
               if (focused) {
-                return <Ionicons name="book" size={29} color={"#2E1966"} />
+                return <Ionicons name="book" size={30} color={"#2E1966"} />;
               }
-              return <Ionicons name="book-outline" size={29} color={"#6939E9"} />
-            }
+              return <Ionicons name="book-outline" size={30} color={"#6939E9"} />;
+            },
           }}
         />
-  
+
         <Tab.Screen
           name="configurações"
           component={StackSettings}
           options={{
             tabBarIcon: ({ color, size, focused }) => {
               if (focused) {
-                return <Ionicons name="settings" size={29} color={"#2E1966"}></Ionicons>
+                return <Ionicons name="settings" size={30} color={"#2E1966"} />;
               }
-              return <Ionicons name="settings-outline" size={29} color={"#6939E9"} />
-            }
+              return <Ionicons name="settings-outline" size={30} color={"#6939E9"} />;
+            },
           }}
         />
       </Tab.Navigator>
-      </Container>
-    );
-  }
-  
+    </Container>
+  );
+}
